a78d713e208f074f054ebfa213328e699ccc0787
Roshni0511 <roshnichauhan051@gmail.com>
2025-09-26 16:11:01 +0530

    add page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Aboutus from './pages/Aboutus';
 
 
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Aboutus' element={<Aboutus/>}/>
       </Routes>
     </>
   );
